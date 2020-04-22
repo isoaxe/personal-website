001120cae5a75a9fbb27370def25a597e06797a3
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-04-22 18:52:41 +0700

    Navbar elements now light up based on where user is on page.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//navElements returns an array nav anchor tags.
+navElements = document.getElementById("navbar").getElementsByTagName("*");
+
+// Expand or contract the dropdown navbar on small screens.
 function toggleExpand() {
   let navbar = document.getElementById("navbar");
   if (navbar.className === "topnav") {
@@ -6,3 +10,58 @@ function toggleExpand() {
     navbar.className = "topnav";
   }
 }
+
+// Helper: Highlights the navbar element based on supplied argument.
+function highlightNavElement(element) {
+  switch (element) {
+    case "home":
+      clearNavHighlights();
+      navElements[0].className = "active";
+      break;
+    case "education":
+      clearNavHighlights();
+      navElements[1].className = "active";
+      break;
+    case "tech":
+      clearNavHighlights();
+      navElements[2].className = "active";
+      break;
+    case "projects":
+      clearNavHighlights();
+      navElements[3].className = "active";
+      break;
+    case "contact":
+      clearNavHighlights();
+      navElements[4].className = "active";
+      break;
+  }
+}
+
+// Helper: Clears all navbar elements of highlighting.
+function clearNavHighlights() {
+  for (i = 0; i < (navElements.length - 2); i++) {
+    navElements[i].className = "";
+  }
+}
+
+// Checks the page position and calls highlightNavElement() based on this.
+function checkPagePosition() {
+  const home = document.getElementById("home").getBoundingClientRect().top;
+  const education = document.getElementById("education").getBoundingClientRect().top;
+  const tech = document.getElementById("tech").getBoundingClientRect().top;
+  const projects = document.getElementById("projects").getBoundingClientRect().top;
+  const contact = document.getElementById("contact").getBoundingClientRect().top;
+  if (contact < 1) {
+    highlightNavElement("contact");
+  } else if (projects < 1) {
+    highlightNavElement("projects");
+  } else if (tech < 1) {
+    highlightNavElement("tech");
+  } else if (education < 1) {
+    highlightNavElement("education");
+  } else {
+    highlightNavElement("home")
+  }
+}
+
+window.onscroll = () => {checkPagePosition()};
