f89118a2bfabaf0b1a500b2f8d9179f73eb0e852
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-10-01 16:10:46 +0700

    Server Routing
    Resolved the file referencing issue so that the website now loads on the local development server and all assets can be found.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
+const path = require('path');
 
+const rootDir = require("./util/path");
 const testRoute = require('./routes/test');
 
-const bodyParser = require('body-parser');
+const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
+
+/* The root directory is the 'backend' folder. This needs to be set to the
+   true root (up one level) in order for files to reference correctly in
+   the local node server (localhost:3000) */
+app.use('/', express.static(path.join(rootDir, "..")));
 
 app.use(testRoute);
 
