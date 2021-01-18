1f7ffb29a45e1d0890b7e8c90cec01a12057d4fa
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-01-18 19:24:37 +0700

    TestComponent: Convert to JSX
    - Convert the TestComponent to JSX.
    - Refer to the preprocessed folder rather than the components folder in index.html.
    - The preprocessing can now be completed by running the jsx script. Confirmed to be working as expected.

--- components/TestComponent/TestComponent.js
+++ components/TestComponent/TestComponent.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const e = React.createElement;
 
 class LikeButton extends React.Component {
   constructor(props) {
@@ -13,10 +12,10 @@ class LikeButton extends React.Component {
       return 'You liked comment number ' + this.props.commentID;
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
     );
   }
 }
