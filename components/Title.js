2b36e5a21756d2f05254b0675e31f730df3989f0
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-02-07 10:22:29 +0700

    Create Title.js
    Create a component that allows the title of each section to slide in.

---
+++ components/Title.js
@@ -0,0 +1,22 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const LightSpeed = require('react-reveal/LightSpeed');
+'use strict';
+
+
+function Title(props) {
+  return (
+    <div>
+      <LightSpeed left>
+        <h2>{props.text}</h2>
+      </LightSpeed>
+    </div>
+  );
+}
+
+// Find all appropriate DOM containers, and render Title components into them.
+document.querySelectorAll('.title-container')
+  .forEach(domContainer => {
+    const text = domContainer.dataset.text;
+    ReactDOM.render(<Title text={text} />, domContainer);
+  });
