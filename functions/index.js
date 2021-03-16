aaf07b1b185f7b2b447ae8e290fa02533a32ff74
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-03-16 09:44:36 +0700

    Index: Add Express routes
    Add Express routes much like the original server.js file in the 'backend' directory.
    Note call to Firebase to initialise app.
    Boilerplate commented out for now.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const rootDir = require("./util/path");
+const homeRoute = require("./routes/home");
+const emailRoute = require("./routes/email");
+
+// Initialize Firebase in order to access its services.
+admin.initializeApp();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+/* The root directory is the 'functions' folder. This needs to be set to the
+   true root (up one level) in order to find the required file (i.e. index.html) */
+app.use("/", express.static(path.join(rootDir, "..")));
+
+app.use(homeRoute);
+app.use(emailRoute);
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
+/*
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+*/
