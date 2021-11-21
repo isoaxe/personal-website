c6da2b4bdfa3e3b7aaec53d8457ec16d82cacc5f
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-11-21 10:34:37 +0000

    Create Contact.js
    Extract the contact form logic into a component. Change class attribute to React syntax and wrap in Zoom component.

---
+++ components/Contact.js
@@ -0,0 +1,30 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+const Zoom = require("react-reveal/Zoom");
+
+
+function Contact () {
+  return (
+    <div>
+      <Zoom delay={1000}>
+        <form action="/submit" method="POST" onSubmit="return validateForm()" name="contact">
+          <input id="name" className="contact-user" type="text" placeholder="Name" name="name" /><br></br>
+          <input id="email" className="contact-user" type="text" placeholder="Email" name="email" /><br></br>
+          <textarea id="message" className="contact-message" placeholder="Message" name="message"></textarea><br></br>
+          <div className="g-recaptcha"
+               data-sitekey="6LfeSREdAAAAAGO14nqrqWFxEc03lAq47TbrNgL6"
+               data-callback="correctCaptcha"
+               data-expired-callback="expiredCaptcha">
+          </div>
+          <button type="submit" className="submit">Submit</button>
+        </form>
+      </Zoom>
+    </div>
+  );
+}
+
+// Find appropriate DOM container, and render Contact component into it.
+document.querySelectorAll(".contact-container")
+  .forEach(domContainer => {
+    ReactDOM.render(<Contact />, domContainer);
+  });
